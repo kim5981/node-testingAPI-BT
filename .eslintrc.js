335c630543f10d761966bbb2c91861f38db78dba
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-26 12:16:29 -0700

    jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
     "env": {
-        "browser": true,
+        "node": true,
         "commonjs": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
     "rules": {
+        "no-unused-vars": 0
     }
 }
